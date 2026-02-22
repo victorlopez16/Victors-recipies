f65efae49f3169f271122e085fdb9c258499595a
victorlopez16 <victorlopezruiz@elpuig.xeill.net>
2026-02-22 18:08:24 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'victor-recipies-def',
+  webDir: 'dist'
+};
+
+export default config;
